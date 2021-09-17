a1dfbe97f3a87e192182b8e797e2fbe8fe0ea2df
maxiaoteng <jizhuwo00@hotmail.com>
2021-09-17 11:48:33 +0800

    部署到github pages, 通过http://notebook.maxiaoteng.xyz/ 访问

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,29 +6,12 @@ module.exports = {
   description: 'Just playing around',
   // 使用主题
   // theme: 'vuepress-theme-xx',
-  // 根路径, 如果是https://foo.github.io/bar/，那么 base 的值就应该被设置为 "/bar/"
+  // 根路径, 用于部署到github, 如果是https://foo.github.io/bar/，那么 base 的值就应该被设置为 "/bar/"
   // base: '/',
   themeConfig: {
-    // .vuepress/public 是默认的base
     logo: '/lion.jpeg',
     navbar: [{ text: '首页', link: '/' }, ...SidebarConfig['NavbarConfig']],
-    // navbar: [
-    //   { text: '首页', link: '/' },
-    //   { text: 'python', link: '/python' },
-    //   { text: 'go', link: '/go' },
-    //   { text: 'guide', link: '/guide' },
-    //   { text: 'vuepress', link: '/vuepress' },
-    // ],
-    // sidebar: 'auto',
-    // sidebar: sidebar,
     sidebar: SidebarConfig['SidebarConfig'],
-    // sidebar: {
-      // sidebarItem
-      // '/vuepress/': [{'text': 'Vuepress', 'link': '/vuepress/', 'children': [{'/config/': [{'text': 'Vuepress config', 'link': '/config/', 'children': [{'text': 'More config', 'link': '/config/more_config.md'}]}]}]}]
-    // },
-    // sidebar: [
-    //   {'text': 'Vuepress', 'link': '/vuepress', 'children': [{'text': 'Vuepress config', 'link': '/vuepress/config', 'children': [{'text': 'More config', 'link': '/vuepress/config/more_config.md'}]}]}
-    // ],
     sidebarDepth: 2,
     lastUpdated: 'Last Updated',
   }
